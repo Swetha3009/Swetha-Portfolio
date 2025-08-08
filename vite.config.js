ff3e0ebf87d6118176e3c5a32fbdbccde17d0034
Swetha3009 <swethajagadeesan2000@gmail.com>
2025-08-07 23:21:43 -0700

    Made changes in config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Change this to match your repo name
 export default defineConfig({
   plugins: [react()],
+  base: '/Swetha-Portfolio/'
 })
